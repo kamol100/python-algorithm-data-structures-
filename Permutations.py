bab87077507a0f85afe741b35d394c78755061da
kamol <kamol@tikweb.dk>
2022-05-12 09:39:54 +0600

    Permutations

---
+++ Permutations.py
@@ -0,0 +1,18 @@
+def permutations(nums):
+    result = []
+
+    if len(nums) == 1:
+        return [nums[:]]
+    for i in range(len(nums)):
+        n = nums.pop(0)
+        perms = permutations(nums)
+        for perm in perms:
+            perm.append(n)
+        result.extend(perms)
+        nums.append(n)
+    return result
+
+
+print(permutations(['1','2','3','4','5']))
+# print(permutations(['1','2','3']))
+# print(permutations(['1']))
