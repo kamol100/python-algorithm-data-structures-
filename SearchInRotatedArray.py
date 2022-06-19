ee73e3719bd179f331cd9d10a40495a304f63254
kamol100 <debnath.kamolkrishna1@gmail.com>
2022-06-19 22:45:31 +0600

    search in rotate array

---
+++ SearchInRotatedArray.py
@@ -0,0 +1,35 @@
+# leetcode 33
+#https://leetcode.com/problems/search-in-rotated-sorted-array/
+def search(nums, target):
+    if nums is None: return -1
+    left = 0
+    right = len(nums) -1
+    while(left < right):
+        mid = left + (right - left) //2
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid
+
+    start = left
+    left = 0
+    right = len(nums) -1
+
+    if target >= nums[start] and target <= nums[right]:
+        left = start
+    else:
+        right = start
+
+    while left <= right:
+        mid = left + (right -left) //2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid
+        else:
+            right = mid
+
+
+
+print(search([4,5,6,7,0,1,2], 1))
+
