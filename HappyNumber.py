c9b8d576be74a73f6e8d5eeb4d9395d7fca96179
kamol100 <debnath.kamolkrishna1@gmail.com>
2022-06-24 07:38:16 +0600

    happy number

---
+++ HappyNumber.py
@@ -0,0 +1,21 @@
+def is_happy(n):
+    visit = set()
+    while n not in visit:
+        visit.add(n)
+        n = sumOfSquares(n)
+        if n == 1:
+            return True
+    return False
+def sumOfSquares(n):
+    output = 0
+    while n:
+        digit = n % 10
+        digit = digit ** 2
+        output += digit
+        n = n//10
+    return output
+
+
+
+
+print(is_happy(7))
