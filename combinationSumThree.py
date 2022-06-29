0588e6693e18478748f5c4d8faf194bea70cee26
kamol100 <debnath.kamolkrishna1@gmail.com>
2022-06-29 07:52:44 +0600

    combination sum three

---
+++ combinationSumThree.py
@@ -0,0 +1,20 @@
+def combination_sum(k, n):
+    res = []
+
+    def backtrack(num, stack, target):
+        if stack and len(stack) == k and target == 0:
+            res.append(stack.copy())
+            return
+        if target <= 0:
+            return
+        for i in range(num + 1, 10):
+            stack.append(i)
+            backtrack(i, stack, target - i)
+            stack.pop()
+
+    backtrack(0, [], n)
+    return res
+
+
+print(combination_sum(3,7))
+print(combination_sum(9,45))
